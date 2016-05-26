68d34fb3195a6d0e50bf10f747c701251f00540c
amanrayat <amanrayat32@gmail.com>
2016-05-26 23:56:06 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##This function is used to get the value of the matrix whenever needed 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+setmat<-function(){
+    a<<-y
+    m<<-NULL
+  }
+  getmat<-function() a
+  calc_inv<-function(a) m<<-solve(a)
+  get_inv<-function() m
 }
 
-
-## Write a short comment describing this function
+#This function is needed to calculate the inverse of a matrix and check whether it is in cache or not 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+m <- a$get_inv()
+  if(!is.null(m)) {
+    message("retriving data")
+    return(m)
+  }
+  ans <- x$getmat()
+  m <- solve(data, ...)
+  a$set_inv(m)}
